45d33901d27ea367c41f664a3d637abadd62d3eb
nandasafiqalfiansyah <nandasafiqalfiansyah@gmail.com>
2026-02-24 17:09:52 +0700

    Initial Commit

---
+++ mathTests/mathTests.swift
@@ -0,0 +1,17 @@
+//
+//  mathTests.swift
+//  mathTests
+//
+//  Created by Nanda Safiq Alfiansyah on 24/02/26.
+//
+
+import Testing
+@testable import math
+
+struct mathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
